e207fad255b6951965d786ffc1cf2a93f56f49a1
duniawael <130253531+duniawael@users.noreply.github.com>
2023-04-08 23:57:58 +0200

    Add files via upload

---
+++ assign_01_poblem_05_code.java
@@ -0,0 +1,18 @@
+
+// dunia wael  id:20216205
+import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long X = input.nextLong();
+        long Y = input.nextLong();
+        
+        System.out.println(X+" + "+Y+" = "+(X+Y));
+        System.out.println(X+" * "+Y+" = "+(X*Y));
+        System.out.println(X+" - "+Y+" = "+(X-Y));
+
+
+    }
+}
